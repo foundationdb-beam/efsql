effa69721dc5f17327b072edf4ef568bf10df72d
Jesse Stimpson <jesse.stimpson@gmail.com>
2025-05-15 22:59:10 -0400

    Convention for querying by unkown primary key field

--- test/by_pk_test.exs
+++ test/by_pk_test.exs
@@ -1,13 +1,15 @@
 defmodule EfsqlTest.Integration.SelectByPk do
   use EfsqlTest.Case, async: true
 
+  # Querying primary key always uses _ identifier because we don't know what the primary key field is without the Schema
+
   test "select by pk equals", context do
     tenant_id = context[:tenant_id]
 
     assert [
              [id: "0001", name: "Alice", notes: "Lorem ipsum"]
            ] =
-             Efsql.all("select id, name, notes from #{tenant_id}.users where id = '0001';")
+             Efsql.all("select id, name, notes from #{tenant_id}.users where _ = '0001';")
   end
 
   test "select by pk between", context do
@@ -18,7 +20,7 @@ defmodule EfsqlTest.Integration.SelectByPk do
              [id: "0002", name: "Bob", notes: "foobar"]
            ] =
              Efsql.all(
-               "select id, name, notes from #{tenant_id}.users where id >= '0001' and id <= '0002';"
+               "select id, name, notes from #{tenant_id}.users where _ >= '0001' and _ <= '0002';"
              )
   end
 end
